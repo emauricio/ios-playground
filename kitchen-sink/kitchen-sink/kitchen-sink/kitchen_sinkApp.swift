a9905b609402cbf1155b6f7bb84db8d0a5167833
Efren Mauricio <efren.mauricio@gmail.com>
2021-03-19 19:30:40 +0100

    create new playground app

---
+++ kitchen-sink/kitchen-sink/kitchen-sink/kitchen_sinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  kitchen_sinkApp.swift
+//  kitchen-sink
+//
+//  Created by Efren Mauricio on 2021/03/19.
+//
+
+import SwiftUI
+
+@main
+struct kitchen_sinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
